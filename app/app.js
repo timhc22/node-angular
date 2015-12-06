670de33a9a2747696dce8e28782a9aed6b04aef2
Timothy Coleman <timhc22@gmail.com>
2015-12-06 23:43:39 +0100

    working basic

--- app/app.js
+++ app/app.js
@@ -1,13 +1,75 @@
-var express = require('express');
-var app = express();
+var express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    bodyParser = require('body-parser'),
+    routes = require('./routes'),
+    app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+// this makes html the engine
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+// view engine setup
+//app.set('views', __dirname + '/views')
+//app.set('view options', { layout:'layout.html' })
+//app.set('view engine', 'html')
+//app.engine('html', hbs.__express)
+//app.use(express.static(__dirname + '/public'))
+//expstate.extend(app)
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false })); // todo what is this
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+// todo should these error handlers be elsewhere?
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error.html', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error.html', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+// run app
 var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
 
     console.log('Example app listening at http://%s:%s', host, port);
 });
+
+module.exports = app;
